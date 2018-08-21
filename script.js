590e58667a9845b9e206ad1415a90d55ab40d1a9
Yaroslav <mbuu8888@gmail.com>
2018-08-22 01:41:21 +0300

    portfolio

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-let cards = document.getElementsByClassName('card');
-let fronts = document.getElementsByClassName('front');
-let backs = document.getElementsByClassName('back');
 
 
-for (let i = 0; i < cards.length; i++) {
-    cards[i].onmouseover = function () {
-        fronts[i].style.transform = 'rotateY(180deg)';
-        backs[i].style.transform = 'rotateY(360deg)';
-    }
-}
