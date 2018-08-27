ff7fa4a3ecda8327e27b6c8aebe09b318c74bb37
Yaroslav Nikiforov <mbuu8888@gmail.com>
2018-08-28 00:53:08 +0300

    portfolio

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 jQuery(document).ready(function () {
 
-    particlesJS.load('particles-js', 'particles.json', function() {
+    particlesJS.load('particles-js', 'particles.json', function () {
         console.log('callback - particles.js config loaded');
     });
 
 });
+
+document.body.onload = function () {
+
+    setTimeout(function () {
+        let preloader = document.querySelector('.preloader');
+        document.body.style.height = 'auto';
+        document.body.style.overflow = 'auto';
+
+        if (!preloader.classList.contains('loaded')) {
+            preloader.classList.add('loaded');
+        }
+
+    }, 1000);
+};
