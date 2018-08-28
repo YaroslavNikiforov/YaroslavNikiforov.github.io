c6a3952644c58e83316697964ef4dc6d94e1a9a5
Yaroslav Nikiforov <mbuu8888@gmail.com>
2018-08-28 19:32:30 +0300

    adding first page

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ document.body.onload = function () {
 
     setTimeout(function () {
         let preloader = document.querySelector('.preloader');
-        document.body.style.height = 'auto';
-        document.body.style.overflow = 'auto';
 
         if (!preloader.classList.contains('loaded')) {
             preloader.classList.add('loaded');
