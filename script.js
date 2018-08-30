c91be516900d24b0b5f160a74ff5e2c27b342b81
Yaroslav Nikiforov <mbuu8888@gmail.com>
2018-08-30 15:10:14 +0300

    edits

--- script.js
+++ script.js
@@ -17,3 +17,59 @@ document.body.onload = function () {
 
     }, 1000);
 };
+
+let keys = document.getElementsByClassName('key');
+
+(function () {
+    let rotateY = 0,
+        rotateX = 0;
+
+    document.onkeydown = function (e) {
+        if (e.keyCode === 37) {
+            rotateY += 90;
+        } else if (e.keyCode === 38) {
+            rotateX -= 90;
+        } else if (e.keyCode === 39) {
+            rotateY -= 90;
+        } else if (e.keyCode === 40) {
+            rotateX += 90;
+        }
+
+        document.querySelector('.cube').style.transform =
+            'rotateY(' + rotateY + 'deg)' +
+            'rotateX(' + rotateX + 'deg)';
+    };
+
+    keys[0].onclick = function (e) {
+        rotateY += 90;
+
+        document.querySelector('.cube').style.transform =
+            'rotateY(' + rotateY + 'deg)' +
+            'rotateX(' + rotateX + 'deg)';
+    };
+
+    keys[1].onclick = function (e) {
+        rotateX -= 90;
+
+        document.querySelector('.cube').style.transform =
+            'rotateY(' + rotateY + 'deg)' +
+            'rotateX(' + rotateX + 'deg)';
+    };
+
+    keys[2].onclick = function (e) {
+        rotateY -= 90;
+
+        document.querySelector('.cube').style.transform =
+            'rotateY(' + rotateY + 'deg)' +
+            'rotateX(' + rotateX + 'deg)';
+    };
+
+    keys[3].onclick = function (e) {
+        rotateX += 90;
+
+        document.querySelector('.cube').style.transform =
+            'rotateY(' + rotateY + 'deg)' +
+            'rotateX(' + rotateX + 'deg)';
+    };
+
+})();
